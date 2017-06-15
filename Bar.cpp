02c6b58ca5805d97c7e0dac5f045402e9fe51f54
Bwambocos <mijyumaru2525@outlook.jp>
2017-06-15 22:22:53 +0900

    Bar: 下描画
    ・背景図形を描画

--- Bar.cpp
+++ Bar.cpp
@@ -3,6 +3,13 @@
 #include "SceneMgr.h"
 #include "Bar.h"
 
+// define
+#define DEFAULT_mainRectWidth 256
+
+static RoundRect fieldRect(0, 0, 768, BAR_HEIGHT, 16);
+static RoundRect mainRect(256, 0, 256, BAR_HEIGHT, 16);
+static int32_t mainRectWidth = DEFAULT_mainRectWidth;
+
 // バー 初期化
 void Bar_Init()
 {
@@ -12,11 +19,13 @@ void Bar_Init()
 // バー 更新
 void Bar_Update()
 {
-
+	mainRectWidth = Max(DEFAULT_mainRectWidth, mainRectWidth);
+	mainRect = RoundRect(256 - (mainRectWidth - DEFAULT_mainRectWidth) / 2, 0, mainRectWidth, BAR_HEIGHT, 16);
 }
 
 // バー 描画
 void Bar_Draw()
 {
-
+	fieldRect.draw(Color(200, 200, 200));
+	mainRect.draw(Color(125, 125, 125));
 }
