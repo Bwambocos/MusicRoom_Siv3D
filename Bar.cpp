cd7b5e8aad12066a4da497ea476ee6826d8b8159
Bwambocos <mijyumaru2525@outlook.jp>
2017-06-15 22:06:48 +0900

    Bar: 下準備
    ・関数の宣言

---
+++ Bar.cpp
@@ -0,0 +1,22 @@
+// include
+#include <Siv3D.hpp>
+#include "SceneMgr.h"
+#include "Bar.h"
+
+// バー 初期化
+void Bar_Init()
+{
+
+}
+
+// バー 更新
+void Bar_Update()
+{
+
+}
+
+// バー 描画
+void Bar_Draw()
+{
+
+}
