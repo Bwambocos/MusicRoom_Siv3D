c82a201d1a540dc361e021a87460eba0e8c88c2a
Bwambocos <mijyumaru2525@outlook.jp>
2017-06-23 14:21:00 +0900

    Detail: 曲リスト 初期化
    ・曲リスト 初期化部 実装

--- Detail.cpp
+++ Detail.cpp
@@ -1,7 +1,20 @@
+// include
 #include <Siv3D.hpp>
 #include "Detail.h"
 #include "Bar.h"
+#include "Select.h"
 
+// 曲リスト 構造体
+struct List
+{
+	bool isPlaying;
+	Sound music;
+	String name;
+	int32_t totalTime;
+	bool isFav;
+};
+
+// グローバル変数
 static Texture main;
 static RoundRect albumImage(25, 25 + BAR_HEIGHT, 250, 250, 12.5);
 static RoundRect albumDetail(325, 25 + BAR_HEIGHT, 393, 250, 12.5);
@@ -9,11 +22,35 @@ static RoundRect albumList_Flag(25, 300 + BAR_HEIGHT, 36, 36, 5);
 static RoundRect albumList_Name(64, 300 + BAR_HEIGHT, 537, 36, 5);
 static RoundRect albumList_Time(604, 300 + BAR_HEIGHT, 100, 36, 5);
 static RoundRect albumList_Fav(707, 300 + BAR_HEIGHT, 36, 36, 5);
+static String albumName, albumCreator, albumExpl;
+static Texture albumImg;
+static std::vector<List>albumList;
 
 // アルバム詳細 初期化
 void Detail_Init()
 {
 	main = Texture(L"data\\Detail\\main.png");
+	albumName = getSetAlbum();
+
+	// 曲リスト 初期化
+	{
+		const String extensions[] = { L".wav",L".ogg",L".mp3" };
+		TextReader reader(L"music\\" + albumName + L"\\music_list.txt");
+		String tempName; Sound tempMusic; int32_t temp_totalTime;
+		while (reader.readLine(tempName))
+		{
+			for (auto ext : extensions)
+			{
+				if (FileSystem::IsFile(L"music\\" + albumName + L"\\" + tempName + L"\\" + tempName + ext))
+				{
+					tempMusic = Sound(L"music\\" + albumName + L"\\" + tempName + L"\\" + tempName + L".mp3");
+					break;
+				}
+			}
+			temp_totalTime = tempMusic.lengthSample();
+			albumList.push_back({ false,tempMusic,tempName,temp_totalTime,false });
+		}
+	}
 }
 
 // アルバム詳細 更新
